c9bd0454ce9584716fd8bcf27355806d965b65ab
vdw <dkrestos@gmail.com>
2012-06-20 15:29:09 +0300

    Add fix for small final sigma.

--- MY_string_helper.php
+++ MY_string_helper.php
@@ -27,20 +27,27 @@ if ( ! function_exists('grstrtoupper'))
 
 		$latin_check = '/[\x{0030}-\x{007f}]/u';
 
-		$string = preg_match($latin_check, $string) ? strtoupper($string) : $string;
+		if (preg_match($latin_check, $string))
+		{
+
+			$string = strtoupper($string);
+
+		}
 
 		$letters  								= array('α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 'χ', 'ψ', 'ω');
 		$letters_accent 						= array('ά', 'έ', 'ή', 'ί', 'ό', 'ύ', 'ώ');
 		$letters_upper_accent 					= array('Ά', 'Έ', 'Ή', 'Ί', 'Ό', 'Ύ', 'Ώ');
 		$letters_upper_solvents 				= array('ϊ', 'ϋ');
+		$letters_other 							= array('ς');
 
 		$letters_to_uppercase					= array('Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω');
 		$letters_accent_to_uppercase 			= array('Α', 'Ε', 'Η', 'Ι', 'Ο', 'Υ', 'Ω');
 		$letters_upper_accent_to_uppercase 		= array('Α', 'Ε', 'Η', 'Ι', 'Ο', 'Υ', 'Ω');
 		$letters_upper_solvents_to_uppercase 	= array('Ι', 'Υ');
+		$letters_other_to_uppercase 			= array('Σ');
 
-		$lowercase = array_merge($letters, $letters_accent, $letters_upper_accent, $letters_upper_solvents);
-		$uppercase = array_merge($letters_to_uppercase, $letters_accent_to_uppercase, $letters_upper_accent_to_uppercase, $letters_upper_solvents_to_uppercase);
+		$lowercase = array_merge($letters, $letters_accent, $letters_upper_accent, $letters_upper_solvents, $letters_other);
+		$uppercase = array_merge($letters_to_uppercase, $letters_accent_to_uppercase, $letters_upper_accent_to_uppercase, $letters_upper_solvents_to_uppercase, $letters_other_to_uppercase);
 
 		$uppecase_string = str_replace($lowercase, $uppercase, $string);
 
